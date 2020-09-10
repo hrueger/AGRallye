699ef6d038f8cfdea982df5cf6b46577becf815a
Hannes Rüger <hannesrueger@gmx.de>
2020-09-10 17:48:55 +0200

    added basic backend ui

--- src/app/_components/backend/backend.component.ts
+++ src/app/_components/backend/backend.component.ts
@@ -1,5 +1,6 @@
 import { Titlebar, Color } from "custom-electron-titlebar";
 import { Component, OnInit } from "@angular/core";
+import { remote } from "electron";
 
 @Component({
     selector: "app-backend",
@@ -7,10 +8,17 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./backend.component.scss"],
 })
 export class BackendComponent implements OnInit {
+    public countdownMode: string;
+    public minutes: number;
+    public zeroHours: number;
+    public zeroMinutes: number;
     ngOnInit(): void {
         const t = new Titlebar({
             backgroundColor: Color.fromHex("#ECECEC"),
         });
-        console.log(t);
+    }
+
+    public updateCountdown(): void {
+        remote.ipcMain.emit("setup-countdown", this.countdownMode, this.countdownMode == "zero" ? [this.zeroHours, this.zeroMinutes] : this.minutes);
     }
 }

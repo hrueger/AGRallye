e4cb7dfd91a1a096fded4e4ed0e67ad3f785ea4b
Hannes Rüger <hannesrueger@gmx.de>
2020-09-10 18:11:25 +0200

    add teams and tasks

--- src/app/_components/backend/backend.component.ts
+++ src/app/_components/backend/backend.component.ts
@@ -1,6 +1,15 @@
 import { Titlebar, Color } from "custom-electron-titlebar";
 import { Component, OnInit } from "@angular/core";
 import { remote } from "electron";
+import Swal from "sweetalert2";
+
+type Team = {
+    name: string,
+    tasks: {
+        task: string,
+        points: number,
+    }[],
+}
 
 @Component({
     selector: "app-backend",
@@ -12,6 +21,10 @@ export class BackendComponent implements OnInit {
     public minutes: number;
     public zeroHours: number;
     public zeroMinutes: number;
+
+    public teams: Team[] = [];
+    public currentTeamIdx: number;
+
     ngOnInit(): void {
         const t = new Titlebar({
             backgroundColor: Color.fromHex("#ECECEC"),
@@ -20,5 +33,32 @@ export class BackendComponent implements OnInit {
 
     public updateCountdown(): void {
         remote.ipcMain.emit("setup-countdown", this.countdownMode, this.countdownMode == "zero" ? [this.zeroHours, this.zeroMinutes] : this.minutes);
+    }
+
+    public async addTeam(): Promise<void> {
+        this.teams.push({
+            name: (await Swal.fire({
+                input: "text",
+                title: "Teamnamen eingeben",
+            }) as any).value,
+            tasks: [],
+        });
+    }
+
+    public async addTask(): Promise<void> {
+        this.teams[this.currentTeamIdx].tasks.push({
+            task: (await Swal.fire({
+                input: "text",
+                title: "Teamnamen eingeben",
+            }) as any).value,
+            points: (await Swal.fire({
+                input: "number",
+                title: "Punkte eingeben",
+            }) as any).value,
+        });
+    }
+
+    public getSum(team: Team): number {
+        return team.tasks.map((t) => t.points).reduce((a, b) => a + b, 0);
     }
 }
